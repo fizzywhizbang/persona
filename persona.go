56e65eea8e2b01256bdf7e0805c73d4f3dd39800
FizzyWhizbang <marc.levine@gmail.com>
2021-10-11 08:58:43 -0500

    update persona
    added option for writing to file and reformatted output text

--- persona.go
+++ persona.go
@@ -3,6 +3,8 @@ package main
 import (
 	"flag"
 	"fmt"
+	"os"
+	"strconv"
 
 	cc "github.com/fizzywhizbang/persona/ccgen"
 	p "github.com/fizzywhizbang/persona/persongen"
@@ -13,13 +15,14 @@ func main() {
 
 	typeFlag := flag.String("t", "visa", "amex, dci, dcu, discover, jcb, mae, maeui, mc, visa")
 	qtyFlag := flag.Int("q", 1, "An integer for quantity")
+	writeToFile := flag.Bool("w", false, "if included it will be true")
 
 	flag.Parse()
 	fmt.Println("")
 	fmt.Println("######################################")
-	fmt.Println("type:", *typeFlag)
-	fmt.Println("qty:", *qtyFlag)
-
+	fmt.Println("-t type:", *typeFlag)
+	fmt.Println("-q qty:", *qtyFlag)
+	fmt.Println("-w write to file:", *writeToFile)
 	fmt.Println("######################################")
 	fmt.Println("")
 
@@ -29,13 +32,37 @@ func main() {
 	for i := 0; i < len(creditCard); i++ {
 		card := creditCard[i]
 		person := p.PersonGen()
-		fmt.Println(card)
-		fmt.Println(person)
+		payload := card.Issuer + "\n"
+		payload += card.Pan.Formatted + "\n"
+		payload += "exp:" + strconv.Itoa(card.ExpiryDate.Month) + strconv.Itoa(card.ExpiryDate.Year) + " CVV:" + card.CVV + "\n"
+		payload += person.First + person.Last + "(" + person.Sex + ")\n"
 		ssn := ss.SSNgen()
 		for !ss.CheckValid(ssn) {
 			ssn = ss.SSNgen()
 		}
-		fmt.Println(ssn)
+		payload += "SSN:" + ssn
+		fmt.Println(payload)
+		fmt.Println("") //line break
+		if *writeToFile {
+			fname := person.First + "_" + person.Last + ".txt"
+			writeToFileFunc(fname, payload)
+		}
+	}
+
+}
+
+func writeToFileFunc(filename string, payload string) {
+	f, err := os.Create(filename)
+	if err != nil {
+		fmt.Println("Could not write file")
+	}
+
+	defer f.Close()
+
+	_, err2 := f.WriteString(payload)
+
+	if err2 != nil {
+		fmt.Println("Could not write to file")
 	}
 
 }
